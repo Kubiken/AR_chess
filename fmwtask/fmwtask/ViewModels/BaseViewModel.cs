2d1b44b90100148a91dcd21cf3ba0415aff60780
Kubiken <Kubiken@yandex.ru>
2018-04-06 03:04:17 +0400

    Framework Task 1.0

---
+++ fmwtask/fmwtask/ViewModels/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace fmwtask.Views
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    
+    }
+}
---
+++ fmwtask/fmwtask/ViewModels/MainViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace fmwtask.Views
+{
+    class MainViewModel:BaseViewModel
+    {
+        #region Props
+        GunContext gc;// Контекст БД
+        private ObservableCollection<Gun> lwContent; //Контент ListView
+        public ObservableCollection<Gun> LwContent
+        {
+            get { return lwContent; }
+            set
+            {
+                lwContent = value;
+                OnPropertyChanged("LwContent");
+            }
+        }
+
+        private Gun selectedItem; // Выбраный в ListView элемент
+        public Gun SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                selectedItem = value;
+                OnPropertyChanged("SelectedItem");
+
+                gc.Entry(SelectedItem).State = EntityState.Modified;
+            }
+        }
+        #endregion
+        #region Commands
+        public IDelegateCommand SaveChanges { get; set; } //Комманда для сохранения изменений
+        void ExecuteSaveChanges(object param)
+        {
+            this.gc.SaveChanges();
+        } //Параметры выполнения комманды SaveChanges
+
+        public IDelegateCommand RemoveChanges { get; set; } //Комманда для отката изменений
+        void ExecuteRemoveChanges(object param)
+        {
+            foreach (DbEntityEntry entry in gc.ChangeTracker.Entries())
+            {
+                if (entry.State == EntityState.Modified)
+                    entry.State = EntityState.Unchanged;
+                SelectedItem = LwContent[0];
+            }
+            
+        } //Параметры выполнения комманды RemoveChanges
+        #endregion 
+        public MainViewModel()
+        {
+            gc = new GunContext();
+            List<Gun> buf = gc.Guns.ToList();
+            LwContent = new ObservableCollection<Gun>(buf);
+            this.SaveChanges = new DelegateCommand(ExecuteSaveChanges);
+            this.RemoveChanges = new DelegateCommand(ExecuteRemoveChanges);
+            
+        }
+    }
+}
